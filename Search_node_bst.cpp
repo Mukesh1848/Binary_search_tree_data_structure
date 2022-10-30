3329ad6da94abbdfc1c88e8a8e85a440eca3aa9a
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-10-30 08:05:21 +0530

    Create Search_node_bst.cpp

---
+++ Search_node_bst.cpp
@@ -0,0 +1,18 @@
+
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+    
+ if(root==NULL){
+     return false;
+ }
+ 
+ if(root->data==x){
+     return true;
+ }
+ else if(root->data > x){
+     search(root->left , x);
+ }else{
+     search(root->right , x);
+ }
+ 
+}
